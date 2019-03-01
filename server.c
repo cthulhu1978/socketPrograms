89529b9b4839b500827582266417fb650163ae55
Isaac Callison <isaacgcallison@gmail.com>
2019-03-01 08:20:58 -0600

    first tut done

--- server.c
+++ server.c
@@ -18,9 +18,24 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  int sockfd, newsockfd, portno;
+  int sockfd, newsockfd, portno, n;
+  char buffer[255];
 
+  struct sockaddr_in serv_addr, cli_addr;
+  socklen_t clilen;
 
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0){ error("Error opening socket");}
+  // use mset
+  bzero( (char*)&serv_addr, sizeof(serv_addr) );
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+    error("Binding failed");
+  }
 
   return 0;
 }
